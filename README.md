c8be922a37a3379c5b3f336b86cbdb91b983da26
szopiks@yahoo.com <szopiks@yahoo.com>
2020-09-24 15:37:25 -0700

    fixing las bug

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+
+***TAP-BAR-REDUX***
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -11,7 +11,7 @@ function KegDetail(props){
       <h3>Price: {keg.price} - Alchohol Content{keg.content}</h3>
       <p><em>{keg.description}</em></p>
       <button onClick={ props.onClickingEdit }>Update Keg</button>
-      <button onClick={ props.onClickingBuy }>Buy a Pint</button>
+      {/* <button onClick={ props.onClickingBuy }>Buy a Pint</button> */}
       <button onClick={()=> onClickingDelete(keg.id) }>Close Keg</button> 
       <hr/>
     </React.Fragment>
